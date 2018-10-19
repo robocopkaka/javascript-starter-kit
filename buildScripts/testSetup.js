c20bf1def0d52a70fd0c849b024cfb9fca2cf00b
Onyekachi Okereke <robocopkaka@gmail.com>
2018-10-19 10:56:53 +0100

    initial commit

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,5 @@
+// register babel to transpile before our tests are run
+require('babel-register')();
+
+// Disable webpack features that Mocha doesn't understand.
+require.extensions['.css'] = function() {};
